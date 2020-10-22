ab6d72b0532d6f47777148b890fa587973857610
tarao1006 <taiga4562@gmail.com>
2020-10-22 17:49:15 +0900

    Change interface to class

--- frontend/src/model/item.ts
+++ frontend/src/model/item.ts
@@ -1,10 +1,30 @@
-export interface Item {
-  id: number
-  categoryId?: number
-  subcategoryId?: number
-  name?: string
-  price?: number
-  count?: number
+const FOOD_CATEGORY_ID = 1;
+const DRINK_CATEGORY_ID = 2;
+
+export class Item {
+  id: number;
+  categoryId?: number;
+  subcategoryId?: number;
+  name?: string;
+  price?: number;
+  count?: number;
+
+  constructor(id: number, categoryId: number, subcategoryId: number, name: string, price: number) {
+    this.id = id;
+    this.categoryId = categoryId;
+    this.subcategoryId = subcategoryId;
+    this.name = name;
+    this.price = price;
+    this.count = 0;
+  }
+
+  isFood = (): boolean => {
+    return this.categoryId === FOOD_CATEGORY_ID;
+  };
+
+  isDrink = (): boolean => {
+    return this.categoryId === DRINK_CATEGORY_ID;
+  };
 }
 
 interface itemDTO {
@@ -16,14 +36,13 @@ interface itemDTO {
 }
 
 export const convertToItem = (object: itemDTO) => {
-  return {
-    id: object.id,
-    categoryId: object.category_id,
-    subcategoryId: object.subcategory_id,
-    name: object.name,
-    price: object.price,
-    count: 0
-  }
+  return new Item(
+    object.id,
+    object.category_id,
+    object.subcategory_id,
+    object.name,
+    object.price
+  )
 }
 
 export const convertToItems = (objects: itemDTO[]) => {
