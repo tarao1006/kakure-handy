ab62d1dd0a08eb4e5a3a913e376535de9be43059
tarao1006 <taiga4562@gmail.com>
2020-11-22 13:35:24 +0900

    Update json

--- dataset/csv_to_json.py
+++ dataset/csv_to_json.py
@@ -1,13 +1,18 @@
 import sys
 import json
+import shutil
 import pandas as pd
 from pathlib import Path
 
 CSV_DIR = Path("./csv")
 JSON_DIR = Path("./json")
+FRONTEND_DIR = Path("../frontend/dataset")
 
 if __name__ == "__main__":
     name = sys.argv[1]
-    c = pd.read_csv(CSV_DIR / f'{name}.csv')
-    with open(JSON_DIR / f'{name}.json', 'w', encoding='utf-8') as f:
+    csv_file = CSV_DIR / f'{name}.csv'
+    json_file = JSON_DIR / f'{name}.json'
+    c = pd.read_csv(csv_file)
+    with open(json_file, 'w', encoding='utf-8') as f:
         json.dump(json.loads(c.to_json(orient="records")), f, indent=2, ensure_ascii=False)
+    shutil.copyfile(json_file, FRONTEND_DIR / json_file.name)
--- frontend/dataset/csv_to_json.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import json
-
-
-if __name__ == "__main__":
-    data = pd.read_csv("./itemsDrink.csv", header=None, names=["subcategory_id", "name", "price"])
-    data.to_json("./itemDrink.json", orient="records", force_ascii=False)
