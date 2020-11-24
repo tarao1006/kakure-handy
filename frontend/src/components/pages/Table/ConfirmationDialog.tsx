969a842a274a45153cc89ec85eec01f4a476d94c
tarao1006 <taiga4562@gmail.com>
2020-11-24 20:51:57 +0900

    Remove waste

--- frontend/src/components/pages/Table/ConfirmationDialog.tsx
+++ frontend/src/components/pages/Table/ConfirmationDialog.tsx
@@ -1,34 +1,11 @@
 import * as React from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import {
   Button,
-  CloseIcon,
-  Collapse,
-  Container,
-  IconButton,
-  List,
-  ListItem,
-  ListItemText,
-  Snackbar,
-  Table,
-  TableBody,
-  TableRow,
-  TableCell,
-  ExpandLess,
-  ExpandMore,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle
 } from '@atoms';
-import { Loading } from '@molecules';
-import { AuthContext } from '../../../contexts/auth';
-import { getTable, exitTable, createBill, deleteBill, updateOrder } from '@api';
-import { Table as TableModel, convertToTable, OrderDetail } from '../../../model';
-import { convertTimeToHM } from '../../../utils';
-import { ModalListItem } from './ModalListItem';
 
 interface ConfirmationDialogProps {
   open: boolean;
@@ -38,13 +15,8 @@ interface ConfirmationDialogProps {
 }
 
 export const ConfirmationDialog: React.FC<ConfirmationDialogProps> = ({open, onClose, topic, subTopic}) => {
-  const handleCancel = () => {
-    onClose(false);
-  }
-
-  const handleExecute = () => {
-    onClose(true);
-  }
+  const handleCancel = () => onClose(false);
+  const handleExecute = () => onClose(true);
 
   return (
     <Dialog
