c37a96127974f39faabd48316a533067b70e9c41
tarao1006 <taiga4562@gmail.com>
2020-11-25 01:26:34 +0900

    Update bill api

--- frontend/src/api/bill.ts
+++ frontend/src/api/bill.ts
@@ -1,6 +1,7 @@
 import * as base from './base';
+import { Bill } from '../model';
 
-export const createBill = async (idToken: string, tableId: string): Promise<any[]> => {
+export const createBill = async (idToken: string, tableId: string): Promise<Bill> => {
   const res = await fetch(`${base.BACKEND_URL}/table/${tableId}/bill`, {
     method: "POST",
     headers: new Headers({
--- frontend/src/model/index.ts
+++ frontend/src/model/index.ts
@@ -1,3 +1,4 @@
+export { Bill, convertToBill } from './bill';
 export { Order, OrderDetail, convertToOrder, convertToOrderDetail } from './order';
 export { Item, convertToItem, convertToItems, MIN_ORDER_COUNT, MAX_ORDER_COUNT } from './item';
 export { Table, TableDTO, convertToTable, convertToTables } from './table';
