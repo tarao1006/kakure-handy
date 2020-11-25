bc72ad7e7d5ed1949239143ec15200715543ef12
tarao1006 <taiga4562@gmail.com>
2020-11-25 18:58:03 +0900

    Update migration

--- database/migration/V10__create_view_table_model.sql
+++ database/migration/V10__create_view_table_model.sql
@@ -2,36 +2,28 @@ CREATE VIEW table_order_amount AS (
 SELECT
   dinner_table.id AS table_id,
   CASE
-    WHEN SUM(table_order.price * table_order.quantity) IS NULL THEN 0
-    ELSE SUM(table_order.price * table_order.quantity)
+    WHEN SUM(cuisine_order.price * cuisine_order.quantity) IS NULL THEN 0
+    ELSE SUM(cuisine_order.price * cuisine_order.quantity)
   END AS amount
 FROM
   dinner_table
 LEFT OUTER JOIN
   (
     SELECT
-      *
+      cuisine_order.table_id,
+      cuisine_order.quantity,
+      menu_item.price
     FROM
-     table_order
+     cuisine_order
+    INNER JOIN
+      menu_item
+    ON
+      cuisine_order.item_id = menu_item.id
     WHERE
       status_id IN (1, 2)
-  ) AS table_order
+  ) AS cuisine_order
 ON
-  dinner_table.id = table_order.table_id
-GROUP BY
-  dinner_table.id
-);
-
-CREATE VIEW table_order_count AS (
-SELECT
-  dinner_table.id AS table_id,
-  COUNT(table_order.order_id) AS order_count
-FROM
-  dinner_table
-LEFT OUTER JOIN
-  table_order
-ON
-  dinner_table.id = table_order.table_id
+  dinner_table.id = cuisine_order.table_id
 GROUP BY
   dinner_table.id
 );
@@ -64,7 +56,6 @@ SELECT
   dinner_table.end_at,
   room.id AS room_id,
   room.name AS room_name,
-  table_order_count.order_count,
   table_order_amount.amount,
   table_bill.bill_id
 FROM
@@ -77,9 +68,6 @@ INNER JOIN
   table_order_amount
 ON
   dinner_table.id = table_order_amount.table_id
-INNER JOIN
-  table_order_count
-USING (table_id)
 INNER JOIN
   table_bill
 USING (table_id)
