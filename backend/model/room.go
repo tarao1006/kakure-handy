2cb80dd55fa83668adbff22dcc9785086c981327
tarao1006 <taiga4562@gmail.com>
2020-11-29 10:23:54 +0900

    Move logic to model

--- backend/model/room.go
+++ backend/model/room.go
@@ -1,7 +1,26 @@
 package model
 
+var ROOM_AVAILABLE_BIT_MASK int64 = 1023
+var roomIDs = []int64{1, 2, 3, 4, 5, 7, 8, 16, 32, 64, 128, 192, 256, 384, 448, 512}
+
 type Room struct {
 	ID       int64  `db:"id" json:"id"`
 	Name     string `db:"name" json:"name"`
 	Capacity int64  `db:"capacity" json:"capacity"`
+}
+
+func AvailableRoomIDs(occupiedTableIDs []int64) []int64 {
+	var filled int64 = 0
+	for _, ID := range occupiedTableIDs {
+		filled |= ID
+	}
+
+	available := ROOM_AVAILABLE_BIT_MASK ^ filled
+	res := make([]int64, 0)
+	for _, ID := range roomIDs {
+		if available&ID == ID {
+			res = append(res, ID)
+		}
+	}
+	return res
 }
--- backend/service/room.go
+++ backend/service/room.go
@@ -2,15 +2,13 @@ package service
 
 import (
 	"github.com/jmoiron/sqlx"
+	"github.com/tarao1006/kakure-handy/model"
 	"github.com/tarao1006/kakure-handy/repository"
 )
 
 type Room struct {
 	db *sqlx.DB
 }
-
-var BIT_MASK int64 = 1023
-var IDs = []int64{1, 2, 3, 4, 5, 7, 8, 16, 32, 64, 128, 192, 256, 384, 448, 512}
 
 func NewRoom(db *sqlx.DB) *Room {
 	return &Room{db: db}
@@ -22,18 +20,12 @@ func (r *Room) AvailableRoom() ([]int64, error) {
 		return nil, err
 	}
 
-	var filled int64 = 0
+	occupiedRoomIDs := make([]int64, 0)
 	for _, table := range tables {
-		filled |= table.Room.ID
-	}
-
-	available := BIT_MASK ^ filled
-	res := make([]int64, 0)
-	for _, ID := range IDs {
-		if available&ID == ID {
-			res = append(res, ID)
+		if !table.IsEnded {
+			occupiedRoomIDs = append(occupiedRoomIDs, table.Room.ID)
 		}
 	}
 
-	return res, nil
+	return model.AvailableRoomIDs(occupiedRoomIDs), nil
 }
